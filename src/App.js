3851e1c3ed045e01a481f19b7d087f9540a06584
Eike mx <merxeike@gmail.com>
2022-04-06 13:08:43 +0100

    added bootstrap styling and title

--- src/App.js
+++ src/App.js
@@ -2,36 +2,36 @@ import React from "react";
 import "./index.css";
 import ToDoComponent from "./components/ToDo";
 
-export default function App() {
+export default function App() 
+{
   const tasklist = [
     {
-        title: "Task 1",
+        task: "Task 1",
         description: "Smile all day.",
         status: "new",
     },
     {
-        title: "Task 2",
+        task: "Task 2",
         description: "Make a nice coffee",
         status: "new",
     },
     {
-        title: "Task 3",
+        task: "Task 3",
         description: "Hug a tree.",
         status: "new",
     }
   ];
 
   return (
-    <div className="App">
-      {tasklist.map((task, index) => {
-        return (
-          <ToDoComponent key={index} data={task} />
-        )
-      })}
-
-      {/* <ToDoComponent data={tasklist[0]} />
-      <ToDoComponent data={tasklist[1]} />
-      <ToDoComponent data={tasklist[2]} /> */}
+    <div className="row justify-content-md-center">
+      <div className="col col-lg-6">
+        <h1 className="title display-1">let's do this</h1>
+        {tasklist.map((task, index) => {
+          return (
+            <ToDoComponent key={index} data={task} />
+          )
+        })}
+      </div>
     </div>
   );
 }
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,14 +1,9 @@
-// import Task from "./Task";
-
 const ToDoComponent = (props) => {
     return (
-        <div className="block">
-            {/* <Task toDoDescription={props.data.description} toDoTask={props.data.title}  /> */}
                 <div className="task-container">
-                <h3 className="task-title">{props.data.title}</h3>
-                <span className="task-description">{props.data.description}</span>
+                <h4 className="task">{props.data.task}</h4>
+                <span className="description">{props.data.description}</span>
             </div>
-        </div>
     );
 };
 
