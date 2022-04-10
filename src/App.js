e4eae58bb81a6e08f48c9c4ab0197bcc7b59420a
predavladc <predavladc@gmail.com>
2022-04-10 10:47:29 +0200

    Added form inputs for to-do task title and description. Delete button also works. Edit button is currently not working. Method should be adding contenteditable property to button on Edit button press.

--- src/App.js
+++ src/App.js
@@ -1,45 +1,97 @@
 import React from "react";
 import "./index.css";
 import ToDoComponent from "./components/ToDo";
+import { useState } from "react";
+import UserInput from "./UserInput";
 
 export default function App() {
-  const tasklist = [
+  const [tasks, setTasks] = useState([
     {
+        id: 1,
         title: "Task 1",
         description: "Smile all day.",
         status: "new",
     },
     {
+        id: 2,
         title: "Task 2",
         description: "Make a nice coffee",
         status: "new",
     },
     {
+        id: 3,
         title: "Task 3",
         description: "Hug a tree.",
         status: "new",
     }
-  ];
+  ])
+
+  const [beingEdited, setIsBeingEdited] = useState(null);
+
+  const handleUserInput = (event) => {
+    setUserInput({ ...userInput, [event.target.name]: event.target.value });
+  };
+
+  const [userInput, setUserInput] = useState({
+    title: "",
+    description: "",
+  });
+
+  const handleAddNewTask = (event) => {
+    console.log("print");
+    event.preventDefault();
+    // console.log("submitting the form");
+    const newTask = {
+      id: new Date().getTime(),
+      title: userInput.title,
+      description: userInput.description,
+      status: "new",
+    };
+    setTasks([...tasks, newTask]);
+
+    setUserInput({
+      title: "",
+      description: "",
+      status: "new",
+    });
+
+    event.target.reset();
+  };
+
+  const handleDeleteTask = (id) => {
+    const filteredTasks = tasks.filter((task) => task.id !== id)
+    setTasks(filteredTasks)
+  }
+
+  const handleEditTask = (id) => {
+    setIsBeingEdited(id)
+  }
+
+  if (beingEdited) {
+    return (
+      <>
+      <h1>EDITION MODE</h1>
+      <input type="text" placeholder="title"></input>
+      <input type="text" placeholder="description"></input>
+      <button>Save</button>
+      </>
+    )
+  }
 
   return (
     <div className="App">
-      {tasklist.map((task, index) => {
+    <UserInput onInput={handleUserInput} onNewTask={handleAddNewTask} />
+
+      {tasks.map((task, index) => {
         return (
-            <div className='listitems'>
-              <ToDoComponent key={index} data={task} />
-              <div className='buttons'>
-                <button type="button" class="btn btn-primary">Edit</button>
-                <button type="button" class="btn btn-danger">Delete</button>
-              </div>
+            <div className='listitems' key={index}>
+              <ToDoComponent data={task} onDelete={handleDeleteTask} onEdit={handleEditTask}/>
+
             </div>
             )
           }
         )
       }
-
-      {/* <ToDoComponent data={tasklist[0]} />
-      <ToDoComponent data={tasklist[1]} />
-      <ToDoComponent data={tasklist[2]} /> */}
     </div>
   );
 }
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -5,9 +5,15 @@ const ToDoComponent = (props) => {
         <div className="block">
             {/* <Task toDoDescription={props.data.description} toDoTask={props.data.title}  /> */}
                 <div className="task-container">
-                <h3 className="task-title">{props.data.title}</h3>
-                <span className="task-description">{props.data.description}</span>
-            </div>
+                    <h3 className="task-title" >{props.data.title}</h3>
+                    <span className="task-description" >{props.data.description}</span>
+
+                </div>
+                <br />
+                <div className='buttons'>
+                <button  button type="button" class="btn btn-primary" onClick={() => props.onEdit(props.data.id)}>Edit</button>
+                <button type="button" class="btn btn-danger" onClick={() => props.onDelete(props.data.id)}>Delete</button>
+              </div>
         </div>
     );
 };
